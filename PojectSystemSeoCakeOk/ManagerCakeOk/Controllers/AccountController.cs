57f216ff4e08fe985d107a1fddefc3f7556cdebf
Tuong.td <tuong.td@vn.vinx.asia>
2023-04-04 17:15:53 +0700

    Update Project CakeOk

---
+++ PojectSystemSeoCakeOk/ManagerCakeOk/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Library.ViewModel.Admin.V_SignIn;
+
+namespace ManagerCakeOk.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(V_Login objLoginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                if (objLoginModel.Username != "duytuong0503@gmail.com" && objLoginModel.Password == "123")
+                {
+                    //Add logic here to display some message to user    
+                    ViewBag.Message = "Invalid Credential";
+                    return View(objLoginModel);
+                }
+                else
+                {
+                    //A claim is a statement about a subject by an issuer and    
+                    //represent attributes of the subject that are useful in the context of authentication and authorization operations.    
+                    var claims = new List<Claim>() {
+                    new Claim(ClaimTypes.NameIdentifier, Convert.ToString("001")),
+                        new Claim(ClaimTypes.Name, "tuong"),
+                        new Claim(ClaimTypes.Role, "Sa"),
+                        new Claim("FavoriteDrink", "Tea")
+                };
+                    //Initialize a new instance of the ClaimsIdentity with the claims and authentication scheme    
+                    var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    //Initialize a new instance of the ClaimsPrincipal with ClaimsIdentity    
+                    var principal = new ClaimsPrincipal(identity);
+                    //SignInAsync is a Extension method for Sign in a principal for the specified scheme.    
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties()
+                    {
+                        IsPersistent = true
+                    });
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(objLoginModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
